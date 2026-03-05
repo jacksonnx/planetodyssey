fd14325919b4bf618a11e13c242a7b7e8b3302c0
jacksonnx <jacksonh.dot@gmail.com>
2026-03-04 18:32:34 -0800

    Fix netlify issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlify from "@netlify/vite-plugin";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlify()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin(), netlify()],
 });
